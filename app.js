eea20e1dc94969f19b13aec63bbd89e7fa7fe554
muhamaad idrees <midreeskhan7276@gmail.com>
2021-11-22 11:44:39 -0800

    commit

---
+++ app.js
@@ -0,0 +1,183 @@
+// question = 1
+
+// function change(){
+//     var user =prompt("text color")
+//     var user1 = prompt("bagraound color")
+//     var par = document.getElementById("para")
+//     par.style.color=user
+//     par.style.backgroundColor=user1
+// }
+
+
+// question = 2
+
+// function show(){
+//  document.getElementById("img").src ='./image/img 1.jpg'
+
+// }
+// function big(){
+//     var bi= document.getElementById("img") 
+//     bi.style.width="700px"
+// }
+
+// function small(){
+//     var sma= document.getElementById("img") 
+//     sma.style.width="200px"
+// }
+
+// function hide(){
+//     document.getElementById("img").src="" 
+// }
+
+// question =3
+
+// function on(){
+//     document.getElementById('bulb').src="./image/on.png"
+// }
+// function off(){
+//     document.getElementById("bulb").src="./image/off.png"
+// }
+
+// function change(){
+// var para = document.getElementsByTagName("p").length
+// // console.log(para)
+// for(var i = 0 ; i <para ;i++){
+//     var user =prompt("enter color" + (i+1) )
+     
+// }
+
+
+// }
+
+       //   chapter = 43 & 48
+ 
+       
+// question =1
+// function alex(){
+
+//     alert("hello world")
+// }
+// question =2
+
+
+// function cover1(){
+// alert("Samsung S5")
+// }
+// function cover2(){
+//     alert("vivo S5")
+// }
+// function cover3(){
+//     alert("Samsung ipro")
+// }
+// function cover4(){
+//     alert("Samsung a3")
+// }
+
+// question =3
+// function delet(){
+//     document.getElementById("nam1").innerHTML=""
+  
+// }
+// function delet1(){
+    
+//     document.getElementById("nam2").innerHTML=""
+// }
+// function delet2(){
+//     document.getElementById("nam3").innerHTML=""
+    
+    
+// }
+// function delet3(){
+//       document.getElementById("nam4").innerHTML=""
+// }
+
+// question =4
+
+// function on(){
+//     document.getElementById('bulb').src="./img/on.png"
+// }
+// function off(){
+//     document.getElementById("bulb").src="./img/off.png"
+// }
+// question =5
+// var clicks = 0;
+// function incr(){
+    
+//     clicks += 1;
+//    var a = document.getElementById("inc").innerHTML = clicks;
+
+// }
+// var clicker = ;
+// function decr(){
+//     clicker -= ;
+//     document.getElementById("inc").innerHTML = clicker;
+// }
+
+
+
+
+
+               //chap 49 -52
+ 
+ 
+               //question 1
+
+// function submit(){
+//     var a = document.getElementById("text")
+//     var b = document.getElementById("tix")
+//     document.write("name  = " +" "+a.value +"<br>" +"password  = " +" "+ b.value)
+// }
+
+//question 2\
+// function read(){
+//     var hmm = document.getElementById("readme")
+// hmm.innerHTML ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque placeat dolores eaque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque placeat dolores eaque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque placeat dolores eaque.<a href =''>hide<a/>"
+// }
+
+
+// question =3 
+
+// function delet(){
+//     document.getElementById("td1").innerHTML=""
+// }
+
+// function delet1(){
+//     document.getElementById("td2").innerHTML=""
+// }
+// function delet2(){
+//     document.getElementById("td3").innerHTML=""
+// }
+// function delet3(){
+//     document.getElementById("td4").innerHTML=""
+
+// }
+// function edit(){
+// var user =prompt("enter name")
+// var user1 =prompt("enter class")
+// var user2 =prompt("enter position")
+
+//     document.getElementById("nam").innerHTML=user
+//     document.getElementById("cll").innerHTML=user1
+//     document.getElementById("pos").innerHTML=user2
+    
+// }
+// function edit1(){
+//     var user =prompt("enter name")
+//     var user1 =prompt("enter class")
+//     var user2 =prompt("enter position")
+    
+//         document.getElementById("nam1").innerHTML=user
+//         document.getElementById("cll2").innerHTML=user1
+//         document.getElementById("pos3").innerHTML=user2
+        
+//     }
+//     function edit2(){
+//         var user =prompt("enter name")
+//         var user1 =prompt("enter class")
+//         var user2 =prompt("enter position")
+        
+//             document.getElementById("nam4").innerHTML=user
+//             document.getElementById("cll5").innerHTML=user1
+//             document.getElementById("pos6").innerHTML=user2
+            
+//         }
